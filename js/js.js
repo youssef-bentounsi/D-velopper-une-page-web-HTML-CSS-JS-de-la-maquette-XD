11ecd580bbda308b5920bb8ea23c401e315b31b3
youssef-bentounsi <bentounsiyoussef12@gmail.com>
2023-02-08 02:03:43 +0100

    up

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 
 
-    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 window.onresize = function() {resize()  }
 
