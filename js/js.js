fcd2e20bb60e1dbd7767d075d339da2fccc682bb
youssef-bentounsi <bentounsiyoussef12@gmail.com>
2023-02-20 00:46:53 +0100

    update

--- js/js.js
+++ js/js.js
@@ -1,81 +1,100 @@
 
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-window.onresize = function() {resize()  }
+//window.onscroll = function() {scrollFunction()};
 
-if (window.innerWidth<992) {
-    document.getElementById("head").classList.add('d-none');
-}
-
-
-function scrollFunction()
+$(document).ready(function ()
 {
-  if (window.innerWidth>992 ) {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100   ) {
-      document.getElementById("head").classList.add('d-none');
-      
-      document.getElementById("logo").classList.remove('d-none');
-      document.getElementById("icons").classList.remove('d-none');
-      
-    } else
-    {
-      document.getElementById("head").classList.remove('d-none');
-      document.getElementById("logo").classList.add('d-none');
-      document.getElementById("icons").classList.add('d-none');
-
+  if ( $(window).width() <992) {
+    $("#head").addClass('d-none');
+    $("#logo").removeClass('d-none');
+    $("header").addClass("header-h");
+  }
+  $(window).scroll(function(){
+    if ($(window).width()>992) {
+      if (document.documentElement.scrollTop > 100 || document.documentElement.scrollTop > 100   ) {
+        $("#head").addClass('d-none');
+        $("#logo").removeClass('d-none');
+        $("#icons").removeClass('d-none');
+        $("header").addClass("header-h");
+        
+      } else
+      {
+        $("#head").removeClass('d-none');
+        $("#logo").addClass('d-none');
+        $("#icons").addClass('d-none');
+        $("header").removeClass("header-h");
+  
+      }
     }
-  }
-  else
-  {
-    document.getElementById("logo").classList.remove('d-none');
-    document.getElementById("head").classList.add('d-none');
-    document.getElementById("icons").classList.add('d-none');
-  }
+    else
+    {
+      $("#logo").removeClass('d-none');
+      $("#head").addClass('d-none');
+      $("#icons").addClass('d-none');
+      $("header").addClass("header-h");
+    }
+  });
+});
+
+
+
+
+
+
+$(".landing-page .slideshow").slick({
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    arrows: true,
+    autoplaySpeed: 4000
+});
+let showBL = 5
+let showNC = 3
+ if (window.innerWidth>1600) {
+   showBL = 5
+   showNC = 2
+}
+else if (window.innerWidth>1400) {
+   showBL = 4
+   showNC = 2
+   
+}
+else if (window.innerWidth>992) {
+   showBL = 3
+   showNC = 2
+   
+   
+}
+else if (window.innerWidth>600) {
+   showBL = 3
+   showNC = 1
+}
+ else if (window.innerWidth>200)
+ {
+   showBL = 1
+   showNC = 1
+     
 }
 
-let items = document.querySelectorAll('.crl .carousel-item')
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+let dot = true;
+if (window.innerWidth<992) {
+  dot=false
+}
+$('.best-seller .slick-carousel').slick({
+    infinite: true,
+    slidesToShow: showBL, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: true, // Adds arrows to sides of slider
+    dots: false // Adds the dots on the bottom
+});
+$('.nos-conseils .slick-carousel').slick({
+    infinite: true,
+    slidesToShow: showNC, // Shows a three slides at a time
+    slidesToScroll: showNC, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: true, // Adds arrows to sides of slider
+    dots: dot // Adds the dots on the bottom
+});
+   
 
-
-// slider
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const carousel = document.querySelector(".carousel-container");
-const track = document.querySelector(".track");
-let width = carousel.offsetWidth;
-let index = 0;
-window.addEventListener("resize", function () {
-  width = carousel.offsetWidth;
-});
-next.addEventListener("click", function (e) {
-  e.preventDefault();
-  index = index + 1;
-  prev.classList.add("show");
-  
-  track.style.transform = "translateX(" + index * -width + "px)";
-  if (track.offsetWidth - index * width < index * width) {
-    next.classList.add("hide");
-  }
-});
-prev.addEventListener("click", function () {
-  index = index - 1;
-  next.classList.remove("hide");
-  if (index === 0) {
-    prev.classList.remove("show");
-  }
-  track.style.transform = "translateX(" + index * -width + "px)";
-});
